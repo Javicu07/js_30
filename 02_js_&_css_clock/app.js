c8ef69b6c6ecc590fb8e672e0b1aca2b75636c53
Javicu07 <javicu89@gmail.com>
2023-09-21 22:44:11 +0200

    main 210908

--- 02_js_&_css_clock/app.js
+++ 02_js_&_css_clock/app.js
@@ -1,11 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const secondHand = document.querySelector('.second-hand');
+    const minuteHand = document.querySelector('.min-hand');
+    const hourHand = document.querySelector('.hour-hand');
 
-const secondHand = document.querySelector('.second-hand');
+    function setDate() {
+        const now = new Date();
 
-function setDate() {
-    const now = new Date();
-    const seconds = now.getSeconds();
-    const secondsDegrees = (seconds * 60);
-    secondHand.element.style.transform = `rotate(${secondsDegrees}deg)`;
-}
+        const seconds = now.getSeconds();
+        const secondsDegrees = ((seconds/60)*360)+90;
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-setInterval(setDate, 1000);
+        const minutes = now.getMinutes();
+        const minutesDegrees = ((minutes/60)*360)+90;
+        minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+        const hours = now.getHours();
+        const hoursDegrees = ((hours/12)*360)+90;
+        hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    }
+
+    setInterval(setDate, 1000);
+})
