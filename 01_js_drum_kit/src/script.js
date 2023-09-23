f353fa1f2a476ebca1842ffae11235754e609f61
Javicu07 <javicu89@gmail.com>
2023-09-23 12:18:49 +0200

    main 230901

--- 01_js_drum_kit/src/script.js
+++ 01_js_drum_kit/src/script.js
@@ -1,22 +1,24 @@
-function removeTransition(event) {
-    console.log(event.propertyName);
-    if(event.propertyName !== 'transform') return;
-    this.target.classList.remove('playing');
-}
+document.addEventListener('DOMContentLoaded', function () {
 
-function playSound(event) {
-    console.log(event.key.toUpperCase());
+    function removeTransition(event) {
+        console.log(event.propertyName);
+        if(event.propertyName !== 'transform') return;
+        this.classList.remove('playing');
+    }
 
-    const audio = document.querySelector(`audio[code = ${event.code}]`);
-    const key = document.querySelector(`.key[code = ${event.code}]`)
-    if(!audio) return;
-    
-    key.classList.add('playing');
-    audio.currentTime = 0;  // rewind to the start, whitout it we don´t init multiple times the keypress 
-    audio.play();
-}
+    function playSound(event) {
+        console.log(event.key.toUpperCase());
 
-const keys = Array.from(document.querySelectorAll(`.key`));
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound)
+        const audio = document.querySelector(`audio[code = ${event.code}]`);
+        const key = document.querySelector(`.key[code = ${event.code}]`)
+        if(!audio) return;
 
+        key.classList.add('playing');
+        audio.currentTime = 0;  // rewind to the start, whitout it we don´t init multiple times the keypress 
+        audio.play();
+    }
+
+    const keys = Array.from(document.querySelectorAll(`.key`));
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+    window.addEventListener('keydown', playSound)
+});
