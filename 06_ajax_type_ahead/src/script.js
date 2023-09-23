0f431d48b875525aab2712e80acc8326bdb199d7
Javicu07 <javicu89@gmail.com>
2023-09-23 20:06:10 +0200

    main 230914

--- 06_ajax_type_ahead/src/script.js
+++ 06_ajax_type_ahead/src/script.js
@@ -9,12 +9,23 @@ fetch(endpoint)
 function findMatches (wordToMatch, cities) {
     return cities.filter(place => {
         const regex = new RegExp(wordToMatch, 'gi'); // global and insentive to upper and lowercase
-        return place.city.match(regex) || place.city.match(regex);
+        return place.city.match(regex) || place.state.match(regex);
     });
 }
 
 function displayMatches() {
     const matchArray = findMatches(this.value, cities);
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        return `
+            <li>
+                <span class="name">${cityName}, ${place.state}</span>
+                <span class="population">${place.population}</span>
+            </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
 }
 
 const searchInput = document.querySelector('.search');
