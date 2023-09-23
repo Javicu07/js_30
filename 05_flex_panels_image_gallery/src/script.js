d7886b332366a40069af0303f2f8fb7d9e4e7894
Javicu07 <javicu89@gmail.com>
2023-09-23 19:08:19 +0200

    main 230910

--- 05_flex_panels_image_gallery/src/script.js
+++ 05_flex_panels_image_gallery/src/script.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const panels = document.querySelectorAll('.panels');
+    const panels = document.querySelectorAll('.panel');
 
     function toggleOpen () {
-        this.classList.toggle('open');
+        if (this.classList.contains('open')) {
+            this.classList.remove('open');
+        } else {
+            panels.forEach(panel => panel.classList.remove('open'));
+            this.classList.toggle('open');
+        }
+    }
+
+    function toggleActive(event) {
+        if (event.propertyName.includes('flex')) {
+            this.classList.toggle('open-active')
+        }
     }
 
     panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-})
+    panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+});
