bf0d9c962ecac1e85487d4671f7313ffe81f6b01
Javicu07 <javicu89@gmail.com>
2023-09-25 20:11:28 +0200

    main 250908

--- 08_fun_with_html_canvas/src/script.js
+++ 08_fun_with_html_canvas/src/script.js
@@ -7,22 +7,27 @@ document.addEventListener('DOMContentLoaded', function () {
     ctx.strokeStyle = '#BADA55';
     ctx.lineJoin = 'round';
     ctx.lineCap = 'round';
-    ctx.lineWidth = 30;
+    //ctx.lineWidth = 30;
 
     let isDrawing = false;
     let lastX = 0;
     let lastY = 0;
+    let hue = 0;
 
     function draw(e) {
         if(!isDrawing) return; // stop function when the mouse is not down
         console.log(e);
+        ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`; 
         ctx.beginPath();
+        ctx.lineWidth = hue/6;
         // start from
         ctx.moveTo(lastX, lastY);
         // go to
         ctx.lineTo(e.offsetX, e.offsetY);
         ctx.stroke();
         [lastX, lastY] = [e.offsetX, e.offsetY];
+        hue++;
+        if(hue >= 360) hue = 0;
     }
 
     canvas.addEventListener('mousemove', draw);
@@ -31,5 +36,5 @@ document.addEventListener('DOMContentLoaded', function () {
         [lastX, lastY] = [e.offsetX, e.offsetY];
     });
     canvas.addEventListener('mouseup', () => isDrawing = false);
-    canvas.addEventListener('mouseout', () => isDrawing = false);
+    canvas.addEventListener('mouseout', () => {isDrawing = false});
 });
