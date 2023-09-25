e9f49a98bdbf07e3f3ce7d360505844a376fea49
Javicu07 <javicu89@gmail.com>
2023-09-25 18:55:00 +0200

    main 250902

--- 07_array_cardio_day_2/src/script.js
+++ 07_array_cardio_day_2/src/script.js
@@ -31,11 +31,19 @@
   const isAllAdult = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
 
   console.log(isAllAdult);
-  
+
   // Array.prototype.find()
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
+  const comment = comments.find(comment => comment.id === 823423);
+
+  console.log(comment);
 
   // Array.prototype.findIndex()
   // Find the comment with this ID
   // delete the comment with the ID of 823423
+  const index = comments.findIndex(comment => comment.id === 823423);
+
+  comments.splice(index, 1);
+
+  console.log(comments);
