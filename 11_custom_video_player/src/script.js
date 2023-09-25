662faa9122cf4c7485721b383675e4c86a59fd58
Javicu07 <javicu89@gmail.com>
2023-09-25 22:56:54 +0200

    main 250922

--- 11_custom_video_player/src/script.js
+++ 11_custom_video_player/src/script.js
@@ -1,16 +1,24 @@
-    /* Get our elements */
-    const player = document.querySelector('.player');
-    const video = player.querySelector('.viewer');
-    const progress = player.querySelector('.progress');
-    const progressBar = player.querySelector('.progress__filled');
-    const toggle = player.querySelector('.toggle');
-    const skipButtons = player.querySelectorAll('[data-skip]');
-    const ranges = player.querySelectorAll('.player__slider');
+/* Get our elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+/* Build out functions */
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
 
-    /* Build out functions */
-    function togglePlay() {
-        const method = video.paused ? 'play' : 'pause';
-        video[method]();
-    }
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+/* Hook up the event listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
 
-    /* Hook up the event listeners */
+toggle.addEventListener('click', togglePlay);
