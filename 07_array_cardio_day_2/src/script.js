6e5ca7a2d73a811f2947c0845e023e7d45c860dd
Javicu07 <javicu89@gmail.com>
2023-09-25 17:55:40 +0200

    main 250901

--- 07_array_cardio_day_2/src/script.js
+++ 07_array_cardio_day_2/src/script.js
@@ -23,10 +23,15 @@
     if (currentYear - person.year >= 19) return true;
   });
   */
-  const isAdult = people.some((person) => ((new Date()).getFullYear()) - person.year);
+  const isAdult = people.some(person => ((new Date()).getFullYear()) - person.year >= 19);
+
+  console.log(isAdult);
 
   // Array.prototype.every() // is everyone 19 or older?
+  const isAllAdult = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
 
+  console.log(isAllAdult);
+  
   // Array.prototype.find()
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
