fd23e7702aad0578a9e0c8c5d866d7d07581f2b9
Javicu07 <javicu89@gmail.com>
2023-09-27 23:38:19 +0200

    main 270910

--- 15_local_storage_&_event_delegation/src/script.js
+++ 15_local_storage_&_event_delegation/src/script.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e) {
     e.preventDefault();
@@ -11,6 +11,7 @@ function addItem(e) {
     }
     items.push(item);
     populateList(items, itemsList);
+    localStorage.setItem('items', JSON.stringify(items));
     this.reset();
 }
 
@@ -18,7 +19,8 @@ function populateList(plates = [], platesList) {
     platesList.innerHTML = plates.map((plate, i) => {
         return `
             <li>
-                <label for="">${plate.text}</label>
+                <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+                <label for="item${i}">${plate.text}</label>
             </li>
         `
     }).join('');
