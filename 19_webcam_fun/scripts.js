8866c78ac0a4afb76e26ccd6c88be056429b59c1
Javicu07 <javicu89@gmail.com>
2023-10-03 19:47:28 +0200

    main 031003

--- 19_webcam_fun/scripts.js
+++ 19_webcam_fun/scripts.js
@@ -58,6 +58,15 @@ function redEffect(pixels) {
     return pixels;
 }
 
+function rgbSplit(pixels) {
+    for (let i = 0; i <= pixels.data.length; i+= 4) {
+        pixels.data[i - 150] = pixels.data[i + 0];   // RED
+        pixels.data[i + 500] = pixels.data[i + 1];   // GREEN
+        pixels.data[i - 550] = pixels.data[i + 2];   // BLUE         
+    }
+    return pixels;
+}
+
 getVideo();
 
 video.addEventListener('canplay', paintToCanvas);
