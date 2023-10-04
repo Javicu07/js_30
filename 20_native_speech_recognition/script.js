ef33d9f6ad1db077980a0738f026184fe15e5686
Javicu07 <javicu89@gmail.com>
2023-10-04 21:14:58 +0200

    main 041003

--- 20_native_speech_recognition/script.js
+++ 20_native_speech_recognition/script.js
@@ -9,7 +9,22 @@ const words = document.querySelector('.words');
 words.appendChild(p);
 
 recognition.addEventListener('result', e => {
-    console.log(e);
+    const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+
+    //console.log(transcript);
+    p.textContent = transcript;
+    if (e.results[0].isFinal) {
+        p = document.createElement('p');
+        words.appendChild(p);
+    }
+    if (transcript.includes('get the Weather')) {
+        console.log('Getting the weather');
+    }
 });
+
+recognition.addEventListener('end', recognition.start);
 
 recognition.start();
