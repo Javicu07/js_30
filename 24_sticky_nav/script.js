b644c4bdd499d006524dd7418ddca702fcff11c4
Javicu07 <javicu89@gmail.com>
2023-10-05 22:18:02 +0200

    main 051006

---
+++ 24_sticky_nav/script.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector('#main');
+const topOfNav = nav.offsetTop;
+
+function fixNav() {
+    window.scrollY >= topOfNav
+        ? document.body.classList.add('fixed-nav')
+        : document.body.classList.remove('fixed-nav')
+}
+
+window.addEventListener('scroll', fixNav);
