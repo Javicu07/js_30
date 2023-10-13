f7014db171fcbf1f9ddae5a035c1160933295458
Javicu07 <javicu89@gmail.com>
2023-10-13 14:35:38 +0200

    main 131001

--- 29_countdown_timer/script.js
+++ 29_countdown_timer/script.js
@@ -1,8 +1,12 @@
 let countdown;
+const timerDisplay = document.querySelector('display__time-left');
+const endTime = document.querySelector('display__end-time');
 
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    displayEndTime(then);
     
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -25,3 +29,11 @@ function displayTimeLeft(seconds) {
     timerDisplay.textContent = display;
 }
 
+function displayEndTime(timespan) {
+    const end = new Date(timespan);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+
+    endTime.textContent = `Be Back At ${adjustedHour}:${minutes}`;
+}
